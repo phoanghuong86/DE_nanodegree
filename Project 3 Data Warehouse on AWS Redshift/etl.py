53c76660ac71a49a0a0594c89816b272753912f1
phoanghuong86 <phoanghuong86@yahoo.com>
2022-01-12 23:57:47 +0700

    Update etl.py
    update elt code

--- Project 3 Data Warehouse on AWS Redshift/etl.py
+++ Project 3 Data Warehouse on AWS Redshift/etl.py
@@ -34,6 +34,7 @@ def insert_tables(cur, conn):
 def main():
     """
     Extract song metadata and user activity data from S3, transform it using a staging table, and load it into fact and dimensional tables for analysis
+    
     """
     
     config = configparser.ConfigParser()
