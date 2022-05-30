0412d41797f776a65bd795135130067180a4f533
Pho Hoang Huong <phoanghuong86@yahoo.com>
2022-05-30 08:02:43 +0700

    format plugin file

--- Project 5 Data Pipelines with Apache Airflow/plugins/__init__.py
+++ Project 5 Data Pipelines with Apache Airflow/plugins/__init__.py
@@ -1,10 +1,7 @@
 from __future__ import division, absolute_import, print_function
-
 from airflow.plugins_manager import AirflowPlugin
-
 import operators
 import helpers
-
 
 # Defining the plugin class
 class UdacityPlugin(AirflowPlugin):
